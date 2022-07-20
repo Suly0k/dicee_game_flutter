4ccc9f3f8b23818afe2eb9eb4700d71a254089cf
szesulyi <98833474+szesulyi@users.noreply.github.com>
2022-07-20 14:25:24 +0200

    Finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(const DicePage());
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void change() {
+    rightDiceNumber = Random().nextInt(6) + 1; //szoki 1-6
+    leftDiceNumber = Random().nextInt(6) + 1;
+  }
 
   // This widget is the root of your application.
   @override
@@ -18,6 +33,52 @@ class DicePage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          change();
+                        },
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(0.0),
+                      ),
+                    ),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        change();
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(0.0),
+                      ),
+                    ),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
